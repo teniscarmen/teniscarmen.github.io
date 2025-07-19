3b1c42781320aabe44c0d3ba1c0f845eaade7ae3
teniscarmen <teniscarmen1@gmail.com>
2025-07-18 22:14:53 -0600

    DE REGRESO

---
+++ js/config.js
@@ -0,0 +1,15 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyCe_HbdWlApCldi2Rk8uituDIwr_RcjAqE",
+  authDomain: "tenis-1baf2.firebaseapp.com",
+  projectId: "tenis-1baf2",
+  storageBucket: "tenischidos",
+  messagingSenderId: "1063958178043",
+  appId: "1:1063958178043:web:01276f257cc083bac1285e",
+  measurementId: "G-XXEEL6LMND"
+};
+
+export const geminiApiKey = "AIzaSyDlYPXBYtJQ_CNGcuijTdX9yIpBZdoepD4";
+
+// URL de la Cloud Function que genera `inventory.json`
+export const inventoryExportEndpoint =
+  "https://us-central1-tenis-1baf2.cloudfunctions.net/exportInventory";
