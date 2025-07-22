4dfb356f1beb0f4e7df10af656131768982511ae
teniscarmen <teniscarmen1@gmail.com>
2025-07-22 10:23:14 -0600

    Fix React root detection

--- src/src/main.jsx
+++ src/src/main.jsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const rootElement =
+  document.getElementById('root') || document.getElementById('root-admin');
+
+if (rootElement) {
+  createRoot(rootElement).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
