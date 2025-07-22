b3d71339e2d38021d524e88c28cd338b1c525c83
teniscarmen <teniscarmen1@gmail.com>
2025-07-22 13:05:32 -0600

    Resume carousel auto-scroll after interaction

--- eslint.config.js
+++ eslint.config.js
@@ -12,6 +12,7 @@ export default [
         window: 'readonly',
         console: 'readonly',
         setTimeout: 'readonly',
+        clearTimeout: 'readonly',
         setInterval: 'readonly',
         Intl: 'readonly',
         Blob: 'readonly',
